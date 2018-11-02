6b6141407e552983613444af011ff06b50ebec2c
James B. Ross <35547276+jbrgfx@users.noreply.github.com>
2018-11-02 06:31:39 -0500

    Update elm.js
    Changed GitHub search to retrieve elm-ui rather than stylish-elephants.

--- elm.js
+++ elm.js
@@ -20893,8 +20893,8 @@ var _jbrgfx$elm_filtered_list$Main$githubSearchQuery = A2(
 				}
 			},
 			{
-				url: 'https://github.com/search?l=&q=stylish-elephants+extension%3Ajson+path%3A%2F&type=Code',
-				label: _mdgriffith$stylish_elephants$Element$text('stylish-elephants repos (github login required)')
+				url: 'https://github.com/search?l=&elm-ui+extension%3Ajson+path%3A%2F&type=Code',
+				label: _mdgriffith$stylish_elephants$Element$text('elm-ui repos (github login required)')
 			}),
 		_1: {ctor: '[]'}
 	});
